999272f9860ae53f7f560aa0ea5f67c73ae768d6
Bhupendra Patel <patel.bhupendra.09ce1062@gmail.com>
2018-10-22 13:40:55 +0530

    added ssl certificates

---
+++ config.js
@@ -0,0 +1,31 @@
+/* 
+ *  Create and export the configuration variables
+ */
+
+// Container for all the environments
+var environments = {};
+
+
+// Staging (default) environment
+environments.staging = {
+    'httpPort': 3000,
+    'httpsPort': 3001,
+    'envName': 'staging'
+}
+
+
+// Production environment
+environments.production = {
+    'httpPort': 5000,
+    'httpsPort': 5001,
+    'envName': 'production'
+}
+
+// Determine which environment was passed as a command-line argument
+var currentEnvironment = typeof (process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check the current environment is one of the environment above, if not, defaulting it to staging
+var environmentToExport = typeof (environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+// Export the object
+module.exports = environmentToExport;
